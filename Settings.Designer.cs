c1ef3e94df7998fef6519bcb2efcaa1909c47af0
Matthew Woerner <mwoerner77@gmail.com>
2022-12-13 20:25:52 -0800

    Renamed project

--- Settings.Designer.cs
+++ Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SVAutoHatch {
+namespace SVEggBot {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
